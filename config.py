00b4d92994d1cea9a042c3e5d38d566af7540321
pap1k <35275000+pap1k@users.noreply.github.com>
2021-09-15 18:23:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = ""
+GROUP_ID_PROD = 145098987
+GROUP_ID = 145098987
+CMD_SYMBOL = '/'
+CONV_TO_LISTEN = 199
+DB_PATH = "manager.db"
+PEER_ADD_NUM = 2000000000
+CONVERSATIONS = {
+    "events" : 176,
+    "flood" : 166,
+    "new" : 165
+}
+CONVERSATIONS_TEST = {
+    "events" : 212,
+    "flood" : 210,
+    "new" : 211
+}
