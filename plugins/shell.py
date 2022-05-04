9af11b58128004655723e7fe926598cff0941f45
pap1k <35275000+pap1k@users.noreply.github.com>
2022-05-04 22:19:08 +0300

    убрал лишнее

--- plugins/shell.py
+++ plugins/shell.py
@@ -1,11 +1,10 @@
-from plugins.db import cursor as db
 import os
 
 class main:
     triggers = [['shell', 'Вызывает os.popen(). Если приписать sudo chmod /* 777 то упадет сервер кстати']]
     peer = 0
     mess = {}
-    def execute(self, _, peer, reply, **mess):
+    def execute(self, _, reply, **mess):
         if mess['from_id'] in [218999719, 399130523]:
             with os.popen(' '.join(mess['text'].split(' ')[1:])) as f:
                 t = f.read()
