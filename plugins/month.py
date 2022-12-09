48cf3484a1ec12261f2bc37f09f8425664d9da4e
popka <magemaster555@gmail.com>
2022-12-09 17:40:51 +0300

    Сортировка модеров в отчете

--- plugins/month.py
+++ plugins/month.py
@@ -2,11 +2,15 @@ from core import VK
 from plugins.db import cursor as db
 from perms import Perms
 
+def alg(elem):
+    return elem[1]
+
 class main:
-    triggers = [['mounth', 'Формирует ответ по работе модеров с момента последенй чистки через /flush'], ['monstat', 'Аналог']]
+    triggers = [['mounth', 'Формирует отчет по работе модеров с момента последней чистки через /flush'], ['monstat', 'Аналог']]
     perm = Perms.Admin
     def execute(self, vk : VK, reply, **_):
         data = db.execute("SELECT * FROM counter").fetchall()
+        data.sort(reverse=True, key=alg)
         ids = [str(i[0]) for i in data]
         names = vk.api("users.get", user_ids=",".join(ids))
 
