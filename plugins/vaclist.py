0b403b444feab792956752534d9c9f7e6d27ba18
pap1k <magemaster555@gmail.com>
2023-06-26 02:17:34 +0300

    Сделал vaclist

---
+++ plugins/vaclist.py
@@ -0,0 +1,21 @@
+from plugins.db import cursor as db
+import datetime
+from perms import Perms
+
+def date(unixtime, format = '%d.%m.%Y %H:%M:%S'):
+    d = datetime.datetime.fromtimestamp(unixtime)
+    return d.strftime(format)
+
+class main:
+    triggers = [['vaclist', 'Показывает список модеров в отпуске + их срок']]
+    perm = Perms.Admin
+    def execute(self, reply, **_):
+        vaclist = db.execute("SELECT * FROM vacation").fetchall()
+        if len(vaclist) > 0:
+            mess = "Список модеров в отпуске:\n\n"
+            for moder in vaclist:
+                mess += f"[id{moder[0]}|Модератор] в отпуске С [{date(moder[1])}] ПО [{date(moder[2])}]\n"
+            reply(mess)
+        else:
+            reply("Список модеров в отпуске пуст")
+        
